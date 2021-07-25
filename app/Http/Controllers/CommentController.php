dc9115c625ee1b00c8165d0496d3c271a18883cd
kartg0203 <kartg0203@gmail.com>
2021-07-25 15:30:08 +0800

    將CommentController的mail功能關掉

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -31,7 +31,7 @@ class CommentController extends Controller
             ];
             //發送郵件，通知作者有新的留言
             // 寫user就好，會自己去找mail，to() 裡面可以傳用戶model/郵箱地址/數組裡面寫多個郵箱地址
-            Mail::to($blog->user)->send(new BlogCommentMail($comment));
+            // Mail::to($blog->user)->send(new BlogCommentMail($comment));
             return response()->json(['state' => true, 'msg' => '回復成功', 'data' => $data]);
         } else {
             return response()->json(['state' => false, 'msg' => '回復失敗']);
